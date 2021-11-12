af5fd4155601c234d5fbf521cff72dcd265b3e3a
Alena Rudenko <a.rudenko@biohimic.local>
2021-11-12 10:51:00 +0300

    camelCase

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import "./App.css";
-import Star from "./components/StarOutline";
 import { ToDoItem } from "./components/todo/ToDoItem";
 
 function App() {
   return (
-    <div className='App'>
-      <div className='Container'>
-        <div className='Header'>
-          <div className='header__container'>
-            <h1 className='Header__logo'>ToDo App</h1>
-            <div className='Header__search'>
-              <input type='search' className='Header__placesearch'></input>
+    <div className="app">
+      <div className="container">
+        <div className="header">
+          <div className="header__container">
+            <h1 className="header__logo">ToDo App</h1>
+            <div className="header__search">
+              <input type="search" className="header__placesearch"></input>
             </div>
-            <div className='Header__button'>
+            <div className="header__button">
               <button>+</button>
             </div>
           </div>
         </div>
-        <div className='Main'>
+        <div className="main">
           <ToDoItem text={"Wake up"} isChecked={true} isFavourite={true} />
           <ToDoItem text={""} isChecked={true} isFavourite={true} />
           <ToDoItem
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__container">
+        <h1 className="header__logo">ToDo App</h1>
+        <div className="header__search">
+          <input type="search" className="header__placesearch"></input>
+        </div>
+        <div className="header__button">
+          <button>+</button>
+        </div>
+      </div>
+    </div>
+  );
+};
