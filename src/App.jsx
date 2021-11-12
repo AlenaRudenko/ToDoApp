785519cbf7461b53b07af04ad0831c93e1ee7a32
Alena Rudenko <a.rudenko@biohimic.local>
2021-11-12 15:53:01 +0300

    buttons edit and trash

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import "./App.css";
+import { Header } from "./components/header/Header";
 import { ToDoItem } from "./components/todo/ToDoItem";
 
 function App() {
   return (
     <div className="app">
       <div className="container">
-        <div className="header">
-          <div className="header__container">
-            <h1 className="header__logo">ToDo App</h1>
-            <div className="header__search">
-              <input type="search" className="header__placesearch"></input>
-            </div>
-            <div className="header__button">
-              <button>+</button>
-            </div>
-          </div>
-        </div>
+        <Header />
         <div className="main">
           <ToDoItem text={"Wake up"} isChecked={true} isFavourite={true} />
-          <ToDoItem text={""} isChecked={true} isFavourite={true} />
+          <ToDoItem text={""} isChecked={false} isFavourite={false} />
           <ToDoItem
             text={"Brush teeth"}
             isChecked={false}
--- src/components/todo/ToDoItem.jsx
+++ src/components/todo/ToDoItem.jsx
@@ -1,58 +1,52 @@
 import { StarFill } from "../StarFill";
 import StarOutline from "../StarOutline";
+import { Pencil } from "./Pencil";
 import "./style.css";
+import { Trash } from "./Trash";
 
 export const ToDoItem = (props) => {
   console.log("props  ", props);
   return (
-    <div className='Main__item'>
-      <div className='Main__group'>
+    <div className="Main__item">
+      <div className="Main__group">
         <div
           className={`Main__point ${
             props.isChecked ? "Main--pointchecked" : ""
-          }`}
-        >
+          }`}>
           {props.isChecked && (
             <svg
-              id='Layer_1'
-              data-name='Layer 1'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 122.88 109.76'
-              width='15px'
-              heigh='15px'
-              fill='#455B74'
-            >
+              id="Layer_1"
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 122.88 109.76"
+              width="15px"
+              heigh="15px"
+              fill="#455B74">
               <defs>
                 <style>.cls-1</style>
               </defs>
               <title>red-check-mark</title>
-              <path d='M0,52.88l22.68-.3A84.1,84.1,0,0,1,46,72.44,314.13,314.13,0,0,1,105.6,0h17.28A522.93,522.93,0,0,0,46.77,109.76C36,86.69,21,67.27,0,52.88Z' />
+              <path d="M0,52.88l22.68-.3A84.1,84.1,0,0,1,46,72.44,314.13,314.13,0,0,1,105.6,0h17.28A522.93,522.93,0,0,0,46.77,109.76C36,86.69,21,67.27,0,52.88Z" />
             </svg>
           )}
-          {/* {props.isChecked ? (
-            <svg
-              id='Layer_1'
-              data-name='Layer 1'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 122.88 109.76'
-              width='15px'
-              heigh='15px'
-              fill='#455B74'
-            >
-              <defs>
-                <style>.cls-1</style>
-              </defs>
-              <title>red-check-mark</title>
-              <path d='M0,52.88l22.68-.3A84.1,84.1,0,0,1,46,72.44,314.13,314.13,0,0,1,105.6,0h17.28A522.93,522.93,0,0,0,46.77,109.76C36,86.69,21,67.27,0,52.88Z' />
-            </svg>
-          ) : undefined} */}
         </div>
-        <div className='Main__task Main--taskchecked'>
-          {props.text || "Эххх пустота к сожалению простите"}
+        <div className={`Main__task ${props.isChecked && "Main--taskchecked"}`}>
+          {props.text ||
+            "Твоя возможность быть продуктивнее ! Поставь задачу :)"}
         </div>
       </div>
-      <div className='Main__fav'>
+      <div className="Main__fav">
         {props.isFavourite ? <StarFill /> : <StarOutline />}
+        <div className="trash__container">
+          <button className="Trash">
+            <Trash />
+          </button>
+        </div>
+        <div className="editor__container">
+          <button className="Editor">
+            <Pencil />
+          </button>
+        </div>
       </div>
     </div>
   );
