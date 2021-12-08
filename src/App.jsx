cfd24babe3a8924f3f53d6d3c03585918178325c
AlenaRudenko <pineapple001@mail.ru>
2021-12-08 21:43:40 +0300

    test state

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const mock = [
     text: "feed cat",
     isChecked: false,
     isFavourite: true,
-    id: "3",
+    id: "4",
     isPushEdit: false,
   },
 ];
@@ -54,22 +54,53 @@ class App extends React.Component {
   handleChange = (event) => {
     this.setState({ todoValue: event.target.value });
   };
+
+  changeChecked3 = (id, field, value) => {
+    this.setState({
+      todos: this.state.todos.map((item) =>
+        item.id === id ? { ...item, [field]: value } : item
+      ),
+    });
+  };
+
+  changeChecked2 = (id, isChecked) => {
+    this.setState({
+      todos: this.state.todos.map((item) =>
+        item.id === id ? { ...item, isChecked } : item
+      ),
+    });
+  };
+
+  changeChecked = (id, newIsCheckedValue) => {
+    this.setState({
+      todos: this.state.todos.map((item) => {
+        if (item.id === id) {
+          return { ...item, isChecked: newIsCheckedValue };
+        } else {
+          return item;
+        }
+      }),
+    });
+  };
   render() {
+    console.log(this.state.todos);
     return (
-      <div className="app">
-        <div className="container">
-          <div className="header">
-            <div className="header__container">
-              <h1 className="header__logo">ToDo App</h1>
+      <div className='app'>
+        <div className='container'>
+          <div className='header'>
+            <div className='header__container'>
+              <h1 className='header__logo'>ToDo App</h1>
               <div
                 className={`header__search ${
                   this.state.isPushedPlus ? "header__search--active" : ""
-                }`}>
-                <div className="header__searchContainer">
+                }`}
+              >
+                <div className='header__searchContainer'>
                   <input
                     onChange={this.handleChange}
-                    type="text"
-                    className={"header__placesearch"}></input>
+                    type='text'
+                    className={"header__placesearch"}
+                  ></input>
                   <AppButton
                     onClickButton={() => {
                       this.setState({ todoValue: this.state.todoValue });
@@ -86,36 +117,39 @@ class App extends React.Component {
                       console.log(this.state.todos);
                       this.setState({ isCreate: !this.state.isCreate });
                       this.setState({ isPushedPlus: !this.state.isPushedPlus });
-                    }}>
+                    }}
+                  >
                     <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      class="bi bi-send-fill"
-                      viewBox="0 0 16 16">
+                      xmlns='http://www.w3.org/2000/svg'
+                      width='16'
+                      height='16'
+                      fill='currentColor'
+                      class='bi bi-send-fill'
+                      viewBox='0 0 16 16'
+                    >
                       <path
-                        fill-rule="evenodd"
-                        d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89.471-1.178-1.178.471L5.93 9.363l.338.215a.5.5 0 0 1 .154.154l.215.338 7.494-7.494Z"
+                        fill-rule='evenodd'
+                        d='M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89.471-1.178-1.178.471L5.93 9.363l.338.215a.5.5 0 0 1 .154.154l.215.338 7.494-7.494Z'
                       />
                     </svg>
                   </AppButton>
                 </div>
               </div>
 
-              <div class="big__button">
+              <div class='big__button'>
                 <AppButton
                   isPushedPlus={this.state.isPushedPlus}
                   onClickButton={() => {
                     this.setState({ isPushedPlus: !this.state.isPushedPlus });
                   }}
-                  size={"large"}>
+                  size={"large"}
+                >
                   +
                 </AppButton>
               </div>
             </div>
           </div>
-          <div className="main">
+          <div className='main'>
             {this.state.todos.map((item) => {
               return (
                 <ToDoItem
@@ -125,6 +159,7 @@ class App extends React.Component {
                   id={item.id}
                   isPushEdit={item.isPushEdit}
                   delTodo={this.deleteTodo}
+                  changeChecked={this.changeChecked}
                 />
               );
             })}
--- src/components/todo/ToDoItem.jsx
+++ src/components/todo/ToDoItem.jsx
@@ -11,7 +11,6 @@ class ToDoItem extends react.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isChecked: props.isChecked,
       isFavourite: props.isFavourite,
       isEdit: false,
       isSent: false,
@@ -24,81 +23,92 @@ class ToDoItem extends react.Component {
   };
   render() {
     return (
-      <div className="Main__content">
+      <div className='Main__content'>
         <div
           className={`Main__item ${
             this.state.isEdit ? "Main__item--active" : ""
-          } `}>
-          <div className="Main__item__firstcontainer">
-            <div className="Main__group">
+          } `}
+        >
+          <div className='Main__item__firstcontainer'>
+            <div className='Main__group'>
               <CheckedButton
-                isChecked={this.state.isChecked}
+                isChecked={this.props.isChecked}
                 onClickButton={() => {
-                  this.setState({ isChecked: !this.state.isChecked });
+                  this.props.changeChecked(
+                    this.props.id,
+                    !this.props.isChecked
+                  );
                 }}
               />
               <div className={`Main__task `}>{this.state.textArea}</div>
             </div>
-            <div className="Main__fav">
+            <div className='Main__fav'>
               <AppButton
                 onClickButton={() => {
                   this.setState({ isFavourite: !this.state.isFavourite });
-                }}>
+                }}
+              >
                 {this.state.isFavourite ? <StarFill /> : <StarOutline />}
               </AppButton>
               <AppButton
                 onClickButton={() => {
                   this.props.delTodo(this.props.id);
-                }}>
+                }}
+              >
                 <Trash />
               </AppButton>
               <AppButton
                 onClickButton={() => {
                   this.setState({ isEdit: !this.state.isEdit });
-                }}>
+                }}
+              >
                 <Pencil />
               </AppButton>
             </div>
           </div>
           {this.state.isEdit && (
-            <div className="Main__item__secondcontainer">
-              <div className="Main__inputContainer">
-                <div className="Main__input">
+            <div className='Main__item__secondcontainer'>
+              <div className='Main__inputContainer'>
+                <div className='Main__input'>
                   <input
-                    type="text"
-                    className="toDoEdit"
-                    onChange={this.handleChange}></input>
+                    type='text'
+                    className='toDoEdit'
+                    onChange={this.handleChange}
+                  ></input>
                 </div>
                 <AppButton
                   onClickButton={() => {
                     this.setState({ textArea: this.state.value });
                     this.setState({ isEdit: !this.state.isEdit });
-                  }}>
+                  }}
+                >
                   <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-send-fill"
-                    viewBox="0 0 16 16">
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='16'
+                    height='16'
+                    fill='currentColor'
+                    class='bi bi-send-fill'
+                    viewBox='0 0 16 16'
+                  >
                     <path
-                      fill-rule="evenodd"
-                      d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89.471-1.178-1.178.471L5.93 9.363l.338.215a.5.5 0 0 1 .154.154l.215.338 7.494-7.494Z"
+                      fill-rule='evenodd'
+                      d='M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89.471-1.178-1.178.471L5.93 9.363l.338.215a.5.5 0 0 1 .154.154l.215.338 7.494-7.494Z'
                     />
                   </svg>
                 </AppButton>
               </div>
-              <div className="Main__tags">
+              <div className='Main__tags'>
                 <AppButton>
                   <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-tags-fill"
-                    viewBox="0 0 16 16">
-                    <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
-                    <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z" />
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='16'
+                    height='16'
+                    fill='currentColor'
+                    class='bi bi-tags-fill'
+                    viewBox='0 0 16 16'
+                  >
+                    <path d='M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z' />
+                    <path d='M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z' />
                   </svg>
                 </AppButton>
               </div>
